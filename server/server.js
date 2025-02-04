975afa4b9d1385e88de1930796509ab0b673ec71
DizValheim <122073923+DizValheim@users.noreply.github.com>
2025-02-04 22:48:03 +0530

    FWA-4 Project Start

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
