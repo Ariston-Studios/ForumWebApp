fffdd438cefc82537953b91039d03a5ac970fc5c
DizValheim <122073923+DizValheim@users.noreply.github.com>
2025-02-07 20:12:19 +0530

    FWA-12 Database Connection

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import pg from "pg";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors());
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT
+});
 
-app.get("/", (req, res) => {
+db.connect();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", async (req, res) => {
+    const result = await db.query("SELECT * FROM users");
+    console.log(result.rows);
     res.sendStatus(200);
 });
 
