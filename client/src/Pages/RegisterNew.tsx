a313e0491c56ce8186ebade6f7a4ae0506864db5
DizValheim <122073923+DizValheim@users.noreply.github.com>
2025-02-17 18:08:00 +0530

    FWA-10 Page Layout & Design

---
+++ client/src/Pages/RegisterNew.tsx
@@ -0,0 +1,44 @@
+import BorderlessForm from "../components/BorderlessForm";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import SignInWithButton from "../components/SignInWithButton";
+
+function Register() {
+  return (
+    <div className="bg-white dark:bg-linear-to-b dark:from-gray-800 from-25% dark:to-fuchsia-950 dark:text-white">
+      <Header />
+      <div className="h-dvh flex flex-col justify-center">
+        <div className="mx-auto mb-20">
+          <h1 className=" text-xl sm:text-2xl md:text-4xl font-playwrite my-2">
+            Create Your [<span className="text-fuchsia-600">Forum</span>]
+            Account Now
+          </h1>
+          <p className="text-center text-gray-400">
+            to get 1% smarter every day.
+          </p>
+        </div>
+        <div className="grid grid-cols-2 divide-x-4 divide-fuchsia-200">
+          <div className="col-span-1">
+          <h1 className="text-white mx-auto text-3xl font-playwrite text-center">
+              Register
+            </h1>
+            <BorderlessForm />
+          </div>
+          <div className="col-span-1 flex flex-col">
+            <h1 className="text-white mx-auto text-3xl font-playwrite text-center">
+              Login With
+            </h1>
+            <div className="justify-center my-auto">
+              <SignInWithButton imgURL="https://img.icons8.com/?size=100&id=17950&format=png&color=FFFFFF" text="Login with Google"/>
+              <SignInWithButton imgURL="https://img.icons8.com/?size=100&id=106567&format=png&color=FFFFFF" text="Login with GitHub"/>
+              <SignInWithButton imgURL="https://img.icons8.com/?size=100&id=30840&format=png&color=FFFFFF" text="Login with Apple"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Register;
