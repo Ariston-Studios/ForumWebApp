0314b93b43d551b87f3faa6f205413f1912d62a6
armandaspatt <armandaspatt@gmail.com>
2025-02-23 00:55:51 +0530

    confirm password removed from LoginForm

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -6,7 +6,6 @@ function BorderlessFormLog() {
     <form className="w-full grid grid-cols-2 gap-x-4 mx-auto" action="">
       <BorderlessInput colSpan="2" name="username/Email" type="text" label="Username/Email"/>
       <BorderlessInput colSpan="2" name="password" type="password" label="Password"/>
-      <BorderlessInput colSpan="2" name="conf-password" type="password" label="Confirm Password"/>
       <button className="outline outline-green-400 hover:bg-green-400 hover:cursor-pointer hover:text-black hover:font-bold transition-all delay-100 col-span-2 mt-4 h-8 rounded-md" type="submit"> Submit </button>
     </form>
   );
