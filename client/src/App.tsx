df78af24776372b2812e32ff44cf87ed108116b1
Ashutosh36474 <149928885+Ashutosh36474@users.noreply.github.com>
2025-03-28 19:41:28 +0530

    Question page Frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,18 @@ import Register from "./Pages/Register.tsx";
 import Login from "./Pages/Login.tsx";
 import SetUsername from "./Pages/SetUsername.tsx";
 import Feed from "./Pages/Feed.tsx";
-import { ThemeProvider } from "@/components/theme-provider"
+
+import Ask from "./Pages/Question.tsx";
 //import SetUsernameModal from "../components/SetUsernameModal.tsx";
-
-
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/set-username" element={<SetUsername />} />
-        <Route path="/feed" element={<Feed />} />
-      </Routes>
-    </ThemeProvider>
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/set-username" element={<SetUsername />} />
+      <Route path="/feed" element={<Feed />} />
+      <Route path="/ask" element={<Ask />} />
+    </Routes>
   );
 }
 
